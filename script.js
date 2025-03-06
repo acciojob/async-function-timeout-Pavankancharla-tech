e1d798efcad7b26dcda90d6b32f14764e7ca454d
Pavankancharla-tech <pavankancharla6@gmail.com>
2025-03-06 21:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async function () {
+    const text = document.getElementById("text").value;
+    const delay = document.getElementById("delay").value;
+    const output = document.getElementById("output");
+    
+    if (!text || !delay) {
+        alert("Please enter both text and delay.");
+        return;
+    }
+
+    
+    function delayMessage(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    
+    async function showMessage() {
+        await delayMessage(Number(delay));
+        output.innerText = text;
+    }
+
+    showMessage();
+});
+
